7c59178fce2f510035b92d231ae5d1412633482f
Swaraj <swarajsingpatil@gmail.com>
2020-09-19 07:43:55 +0530

    app files

--- electron-app/app/app/Routes.tsx
+++ electron-app/app/app/Routes.tsx
@@ -4,24 +4,25 @@ import { Switch, Route } from 'react-router-dom';
 import routes from './constants/routes.json';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
+import LoginPage from './containers/LoginPage';
 
 // Lazily load routes and code split with webpack
-const LazyCounterPage = React.lazy(() =>
-  import(/* webpackChunkName: "CounterPage" */ './containers/CounterPage')
-);
+// const LazyCounterPage = React.lazy(() =>
+//   import(/* webpackChunkName: "CounterPage" */ './containers/CounterPage')
+// );
 
-const CounterPage = (props: Record<string, any>) => (
-  <React.Suspense fallback={<h1>Loading...</h1>}>
-    <LazyCounterPage {...props} />
-  </React.Suspense>
-);
+// const CounterPage = (props: Record<string, any>) => (
+//   <React.Suspense fallback={<h1>Loading...</h1>}>
+//     <LazyCounterPage {...props} />
+//   </React.Suspense>
+// );
 
 export default function Routes() {
   return (
     <App>
       <Switch>
-        <Route path={routes.COUNTER} component={CounterPage} />
-        <Route path={routes.HOME} component={HomePage} />
+        <Route path={routes.LOGIN} component={LoginPage} />
+        <Route path={routes.HOME} component={HomePage} exact/>
       </Switch>
     </App>
   );
--- electron-app/app/app/components/Home.tsx
+++ electron-app/app/app/components/Home.tsx
@@ -1,13 +1,76 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import routes from '../constants/routes.json';
-import styles from './Home.css';
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import routes from '../constants/routes.json';
+// import styles from './Home.css';
 
-export default function Home(): JSX.Element {
+// const clipboard = require('electron-clipboard-extended')
+ 
+// clipboard
+// .on('text-changed', () => {
+//     let currentText = clipboard.readText()
+//     console.log(currentText)
+// })
+// .once('text-changed', () => {
+//     console.log('TRIGGERED ONLY ONCE')
+// })
+// .on('image-changed', () => {
+//     let currentIMage = clipboard.readImage()
+// })
+// .startWatching();
+
+
+
+// function Home():JSX.Element{
+//   return (
+//   <div className={styles.container} data-tid="container">
+//     <h2>Home</h2>
+//     {/* <Link to={routes.COUNTER}>to Counter</Link> */}
+//   </div>
+// );
+// }
+
+// export default Home
+import React, { useState } from "react";
+import Header from "./Header";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function Home() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
-    <div className={styles.container} data-tid="container">
-      <h2>Home</h2>
-      <Link to={routes.COUNTER}>to Counter</Link>
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
     </div>
   );
 }
+
+export default Home;
+
+
