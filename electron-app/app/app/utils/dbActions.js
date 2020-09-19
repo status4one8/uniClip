f0db5a888fbf6b014fd26cd3dc938897c379f58f
YashShettigar <47048488+YashShettigar@users.noreply.github.com>
2020-09-19 16:36:32 +0530

    made few changes to the overall functions

--- electron-app/app/app/utils/dbActions.js
+++ electron-app/app/app/utils/dbActions.js
@@ -7,121 +7,20 @@ firebase.initializeApp(config)
 
 const db = firebase.firestore()
 
-// creating a new account
-export const createUser = async () => {
-    await db.collection('users').add({
-        name: '',
-        username: '',
-        password: ''
+export const addToClipboard = async ({id, clipboardData}) => {
+    await db.collection(`clipboard/${id}/contents`).add({
+        ...clipboardData,
+        deviceType: "PC", 
+        deviceName: "POCO F1",
+        time: Date.now()
     })
-    .then((res) => {
-        console.log('Successfully created an account!')
-    })
-    .catch((err) => {
-        console.log('Error creating an account!')
-    })
+    .then(() => console.log('Copied successfully!'))
+    .catch(() => console.log('Unable to copy!'))
 }
 
-
-// login into an account
-export const loginUser = async () => {
-    await db.collection('users').get()
-    .then((querySnapshot) => {
-        querySnapshot.forEach(({data}) => {
-            if (data.name === '' && data.username === '') {
-                // check for pass
-            }
-
-        })
-    })
-
+export const removeFromClipboard = async ({id, contentId}) => {
+    await db.collection(`clipboard/${id}/contents`).doc(contentId).delete()
+    .then(() => console.log('Removed 1 item from clipboard successfully!'))
+    .catch(() => console.log('Unnable to remove item from the clipboard!'))
 }
 
-
-// store a file
-export const storeCopied = async () => {
-    const date = new Date()
-
-    // upload file to drive if any with file_name as [ORIGINAL_FILE_NAME]+[NAME]+'&'+[USERNAME]
-
-    // create a file_storage
-    await db.collection('file_storage').add({
-        file_name: '',      // in the format [ORIGINAL_FILE_NAME]+[NAME]+'&'+[USERNAME]
-        drive_ref: ''       // drive link
-    })
-    .then((res) => {
-        // create new collection called file_info
-        await db.collection('users').get()
-        .then(async (querySnapshot)=> {
-            querySnapshot.forEach((doc) => {
-                if (doc.data.name === '' && doc.data.username === '') {
-                    // add information about the file and map it to a specific user
-                    await db.collection(`users/${doc.id}/file_info`).get()
-                    .then(async (querySnapshotForFileInfo) => {
-                        // To ensure that there's only one and only one file or text copied for any user
-                        await db.collection(`users/${doc.id}/file_info/`).doc(`${querySnapshotForFileInfo[0].id}`)
-                        .update({
-                            file_name: '',    // latest ORIGINAL_FILE_NAME
-                            file_ref: 'file_storage/'+res.id,   // latest
-                            file_type: '',  // latest
-                            timestamp: {
-                                date: String(date.getDate()).padStart(2, '0')+String(date.getMonth()+1).padStart(2, '0')+date.getFullYear(),
-                                time: date.getHours()+date.getMinutes()+date.getSeconds()
-                            }
-                        })
-                        .then((res) => console.log('Recent file or text updated!'))
-                        .catch(err => {
-                            // there wasn't any file_info collection
-                            await db.collection(`users/${doc.id}/file_info`).add({
-                                file_name: '',    // ORIGINAL_FILE_NAME
-                                file_ref: 'file_storage/'+res.id,
-                                file_type: '',
-                                timestamp: {
-                                    date: String(date.getDate()).padStart(2, '0')+String(date.getMonth()+1).padStart(2, '0')+date.getFullYear(),
-                                    time: date.getHours()+date.getMinutes()+date.getSeconds()
-                                }
-                            })
-                            .then(() => console.log('File or text copied successfully!'))
-                            .catch(() => console.log('Unable to copy the file or text!'))
-                        })
-                    })
-                }
-            })
-        })
-        .catch(err => console.log('User not found!'))
-    })
-    .catch((err) => {
-        console.log('Unable to copy the file or text!')
-    })    
-}
-
-
-// retrieve the file
-
-export const getFile = async () => {
-    await db.collection('users').get()
-    .then(async (querySnapshotForUser) => {
-        querySnapshotForUser.forEach((doc) => {
-            if (doc.data.name === '' && doc.data.username === '') {
-                // gets the file copied recently
-                await db.collection(`users/${doc.id}/file_info`).get()
-                .then(async (querySnapshotForFileInfo) => {
-                    querySnapshotForFileInfo.forEach(({data}) => {
-                        // checks for file_name in file_storage collecction
-                        await db.collection('file_storage').get()
-                        .then(async (querySnapshot3) => {
-                            querySnapshot3.forEach((file_doc) => {
-                                if (file_doc.data.file_name === `${data.file_name+doc.name}&${doc.username}`)
-                                    // get drive link
-                                    const drive_link = file_doc.data.drive_ref
-                            })
-                        })
-                        .catch(err => console.log('No file found on the drive!'))
-                    })
-                })
-                .catch(err => console.log('File doesn\'t exist!'))
-            }
-        })
-    })
-    .catch(err => console.log('Unable to get to the collection user!'))
-}
