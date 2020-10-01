7ca83c732e1ad93e56d591c876c2b40dcb2d5c1a
Ajesh DS <ajeshd17@gmail.com>
2020-10-01 11:01:49 +0530

    Implement Search for clips

--- reactnative-app/app/src/screens/Home.js
+++ reactnative-app/app/src/screens/Home.js
@@ -8,33 +8,57 @@ import {
     Image,
     useWindowDimensions,
 } from 'react-native';
-import { Text, FAB } from 'react-native-paper';
+import { Text, FAB, ActivityIndicator, Searchbar } from 'react-native-paper';
 import auth from '@react-native-firebase/auth';
 import { useAuth } from '../context';
 import ClipboardCard from '../components/ClipboardCard';
 import firestore from '@react-native-firebase/firestore';
 
 const Home = (props) => {
-    const  displayName = auth()?.currentUser?.displayName;
+    const displayName = auth()?.currentUser?.displayName;
     StatusBar.setBarStyle('dark-content');
     const windowWidth = useWindowDimensions().width;
     const windowHeight = useWindowDimensions().height;
+    const [search, setSearch] = useState('');
     const { user } = useAuth();
     const [clipboard, setClipboard] = useState([]);
+    const [filteredClipboard, setFileteredClipboard] = useState(new Set());
+    const [loading, setLoading] = useState(false);
+
     useEffect(() => {
+        setLoading(true);
         firestore()
             .collection(`clipboard/${user.uid}/contents`)
             .orderBy('time', 'desc')
             .onSnapshot((docs) => {
                 if (!docs) return;
-                setClipboard(
-                    docs.docs.map((d) => ({
+                const result = [];
+                const filteredResult = new Set();
+                docs.docs.map((d) => {
+                    const clip = {
                         ...d.data(),
                         id: d.id,
-                    })),
-                );
+                    };
+                    result.push(clip);
+                    filteredResult.add(d.id);
+                });
+                setClipboard(result);
+                setFileteredClipboard(filteredResult);
+                setLoading(false);
             });
     }, []);
+
+    const searchClips = (query) => {
+        setSearch(query);
+        console.log(query);
+        const filtered = new Set();
+        clipboard.forEach((clip) => {
+            if (!clip.isImage && clip.content.toLowerCase().includes(query)) {
+                filtered.add(clip.id);
+            }
+        });
+        setFileteredClipboard(filtered);
+    };
 
     // const clipboard = [
     //     {
@@ -58,46 +82,57 @@ const Home = (props) => {
     return (
         <>
             <FAB
-            style={styles.fab}
-            icon="logout"
-            onPress={async () => {
-                await auth().signOut();
-            }}
-        />
-        <ScrollView
-            contentContainerStyle={{
-                paddingBottom: clipboard.length === 0 ? 0 : 100,
-                paddingHorizontal: 10,
-            }}
-            style={styles.container}>
-           
-            <Text style={styles.greet}>Welcome, {displayName}</Text>
-            <Text style={styles.subtitle}>Your Shared Clipboard</Text>
-            {clipboard.length > 0 && (
-                <View>
-                    {clipboard.map((clip) => (
-                        <ClipboardCard key={clip.id} clip={clip} />
-                    ))}
-                </View>
-            )}
-            {clipboard.length === 0 && (
-                <View
-                    style={{
-                        height: windowHeight - 100,
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                    }}>
-                    <Image
-                        source={require('../images/empty.png')}
+                style={styles.fab}
+                icon="logout"
+                onPress={async () => {
+                    await auth().signOut();
+                }}
+            />
+            <ScrollView
+                contentContainerStyle={{
+                    paddingBottom: clipboard.length === 0 ? 0 : 100,
+                    paddingHorizontal: 10,
+                }}
+                style={styles.container}>
+                <Text style={styles.greet}>Welcome, {displayName}</Text>
+                <Text style={styles.subtitle}>Your Shared Clipboard</Text>
+                {clipboard.length > 0 && (
+                    <View>
+                        <View style={styles.searchContainer}>
+                            <Searchbar
+                                placeholder="Search Clips"
+                                onChangeText={searchClips}
+                                value={search}
+                            />
+                        </View>
+                        {clipboard
+                            .filter((e) => filteredClipboard.has(e.id))
+                            .map((clip) => (
+                                <ClipboardCard key={clip.id} clip={clip} />
+                            ))}
+                    </View>
+                )}
+                {!loading && clipboard.length === 0 && (
+                    <View
                         style={{
-                            width: 300,
-                            height: 300,
-                        }}
-                    />
-                    <Text>Go ahead add some clips!</Text>
-                </View>
-            )}
-        </ScrollView>
+                            height: windowHeight - 100,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}>
+                        <Image
+                            source={require('../images/empty.png')}
+                            style={{
+                                width: 300,
+                                height: 300,
+                            }}
+                        />
+                        <Text>Go ahead add some clips!</Text>
+                    </View>
+                )}
+                {loading && (
+                    <ActivityIndicator size={50} style={{ marginTop: 40 }} />
+                )}
+            </ScrollView>
         </>
     );
 };
@@ -127,5 +162,8 @@ const styles = StyleSheet.create({
         bottom: 20,
         right: 20,
         zIndex: 100,
+    },
+    searchContainer: {
+        marginBottom: 20,
     },
 });
